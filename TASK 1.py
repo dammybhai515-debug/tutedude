b754b7fd424e82ee4e96f4cff3783193ce6260d4
dammybhai515-debug <dammybhai515@gmail.com>
2025-12-09 18:29:21 +0530

    Add files via upload

---
+++ TASK 1.py
@@ -0,0 +1,10 @@
+'''Perform Basic Mathematical Operations
+'''
+a=float(input('Enter the first number :'))
+b=float(input('enter the second number :'))
+
+
+print("Addition of a and b is :" , a+b)
+print("Subtraction of a and b is :" , a-b)
+print("multiplication of a and b is :" , a*b)
+print("division of a and b is :" , a/b)
---
+++ TASK 2.py
@@ -0,0 +1,4 @@
+name=str(input("Enter your first name :"))
+lastname=str(input("Enter your first last name :"))
+
+print("hallo" ,name,lastname,"! Welcome to python programing")
